4eb1d367acceab9f845b685d6903216c2dd3e0c6
Matt <mw4854@nyu.edu>
2022-11-02 14:48:47 -0400

    outline

---
+++ Assets/__Scripts/Services.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Services
+{
+    public static GameManager Game;
+    public static AudioManager Audio;
+    public static ProjectileManager Projectiles;
+    public static RockManager Rocks;
+}
