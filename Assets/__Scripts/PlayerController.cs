7f7c374fe52521cd095f7ed4ef7960cee0e58d5f
Matt <mw4854@nyu.edu>
2022-11-05 01:40:36 -0400

    gonna try something totally different

--- Assets/__Scripts/PlayerController.cs
+++ Assets/__Scripts/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject pointer;
+    public GameObject pointerPivot;
     public float pointerRotationLimit = 90f;
 
     public float shootInterval = 0.25f;
-    public float shootForce = 5f;
+    public float minShootForce = 1f;
+    public float maxShootForce = 10f;
+
+    private bool shooting;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +24,18 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector3 cursorPosition = Services.Cursor.position;
-        Vector3 toCursor = cursorPosition - pointer.transform.position;
+        Vector3 toCursor = cursorPosition - pointerPivot.transform.position;
 
         // AIM
-        float angle = Vector3.SignedAngle(pointer.transform.up, toCursor, Vector3.forward);
-        pointer.transform.Rotate(new Vector3(0f, 0f, angle));
-        float z = pointer.transform.localEulerAngles.z;
+        float angle = Vector3.SignedAngle(pointerPivot.transform.up, toCursor, Vector3.forward);
+        pointerPivot.transform.Rotate(new Vector3(0f, 0f, angle));
+        float z = pointerPivot.transform.localEulerAngles.z;
         if (z > 180f) z -= 360f;
         z = Mathf.Clamp(z, -pointerRotationLimit, pointerRotationLimit);
-        pointer.transform.localEulerAngles = new Vector3(0f, 0f, z);
+        pointerPivot.transform.localEulerAngles = new Vector3(0f, 0f, z);
 
         // SHOOT
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !shooting)
         {
             StartCoroutine(ShootRoutine());
         }
@@ -45,13 +49,18 @@ public class PlayerController : MonoBehaviour
     
     private IEnumerator ShootRoutine()
     {
+        shooting = true;
         while (Input.GetMouseButton(0))
         {
+            Vector3 cursorPosition = Services.Cursor.position;
+            Vector3 toCursor = cursorPosition - pointerPivot.transform.position;
+
             Vector3 position = pointer.transform.position;
             float z = pointer.transform.eulerAngles.z;
-            Vector3 initialForce = shootForce * pointer.transform.up;
+            Vector3 initialForce = Mathf.Clamp(toCursor.magnitude, minShootForce, maxShootForce) * pointer.transform.up;
             Services.Projectiles.CreateProjectile(position, z, initialForce);
             yield return new WaitForSeconds(shootInterval);
         }
+        shooting = false;
     }
 }
